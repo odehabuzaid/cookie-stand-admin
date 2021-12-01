5c8b1482c1d40ba4e11c8e31fb74f7223959b295
Odeh <abuzaid163@hotmail.com>
2021-12-02 01:35:23 +0200

    lab39 - complete

--- pages/components/CookieStandAdmin/report/ReportTable.js
+++ pages/components/CookieStandAdmin/report/ReportTable.js
@@ -1,5 +1,4 @@
 import { hours } from '../../../../data'
-
 const ReportTable = ({ data, onDelete }) => {
   const hourly_sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   const totals = []
@@ -21,9 +20,25 @@ const ReportTable = ({ data, onDelete }) => {
               return (
                 <tr>
                   <td>
-                    <div>
-                      <p>{stand.location}</p>
-                      <span onClick={() => onDelete(stand.id)}>[X]</span>
+                    <div className='flex justify-between p-4'>
+                      <span className='flex'>{stand.location}</span>
+                      <span
+                        className='flex col-end-2'
+                        onClick={() => onDelete(stand.id)}
+                      >
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          className='w-5 h-5 cursor-pointer'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                        >
+                          <path
+                            fill-rule='evenodd'
+                            d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
+                            clip-rule='evenodd'
+                          />
+                        </svg>
+                      </span>
                     </div>
                   </td>
                   {hours.map((_, index) => {
